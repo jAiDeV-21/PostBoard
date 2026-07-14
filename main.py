7ad69a0ccc12fd8d2587621edf06afecfd5359fc
jaidev-21 <jaidevs2107@gmail.com>
2026-07-14 12:21:44 +0530

    use \'response_class\' and \'include_in_schema\' for routes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 posts: list[dict] = [
     {
         "id": 1,
-        "author": "Jaidev Sondgar",
+        "author": "Jaidev Sondagar",
         "title": "FastAPI",
         "content": "Ligth-weight async web framework",
         "date_posted": "April 18, 2026",
@@ -20,9 +21,10 @@ posts: list[dict] = [
 ]
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "Hello World!"}
+    return f"<h1>{posts[0]['author']}</h1>"
 
 @app.get("/api/posts")
 def get_posts():
