198a0ecf997ba15a7b5147a5f40e5799d4b2d3dc
jaidev-21 <jaidevs2107@gmail.com>
2026-08-01 12:10:06 +0530

    add token and user dependencies

--- dependencies.py
+++ dependencies.py
@@ -3,6 +3,10 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from auth import oauth2_scheme, get_current_user
+from models import User
 from database import get_db
 
 db_dependency = Annotated[AsyncSession, Depends(get_db)]
+token_dependency = Annotated[str, Depends(oauth2_scheme)]
+current_user_dependency = Annotated[User, Depends(get_current_user)]
